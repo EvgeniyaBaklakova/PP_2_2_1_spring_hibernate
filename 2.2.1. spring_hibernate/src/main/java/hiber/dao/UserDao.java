bcd7727f9b0918077e2f22834f647c0952368785
EvgeniyaBaklakova <121979488+EvgeniyaBaklakova@users.noreply.github.com>
2023-03-04 17:03:45 +0300

    added car class

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -6,9 +6,9 @@ import hiber.model.User;
 import java.util.List;
 
 public interface UserDao {
-   void add(User user);
-   List<User> listUsers();
-   void addCar(Car car);
-   List<Car> listCar();
-   User getUserByIdCar(String model, int series);
+   void saveUser(User user);
+   List<User> getListUsers();
+   void saveCar(Car car);
+   List<Car> getListCars();
+   User getUserByModelAndSeriesCar(String model, int series);
 }
